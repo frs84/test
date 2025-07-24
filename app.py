33c680c32a904e7f88dbe86ae1b57c2f881ea390
Bfrs <49400641+frs84@users.noreply.github.com>
2025-07-24 15:55:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from openai import OpenAI
+import time
 st.title("💬 Parli italiano ?")
 
 def login():
@@ -15,44 +16,53 @@ def login():
                 st.warning("Bel tentativo, ma no.")
                 st.stop()
             elif check.strip().lower() == "veronica":
-                st.subheader("Benvenuta Veronica")
+                st.subheader(" Benvenuta Veronica 😊 !")
                 st.session_state.checked = True
+                time.sleep(2)
+                
+def chat():
+    # Récupération de la clé API depuis les secrets
+    client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+    # Set a default model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+    
+    if prompt:= st.text_input("Ton message :"):
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display user message in chat message container
+        with st.chat_message("user"):
+            st.markdown(prompt)
+    
+    # Display assistant response in chat message container
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+
 def app():
     if not st.session_state.get("checked",False):
         login()
+    
+    chat()
+
+
 
 app()
 
-# Récupération de la clé API depuis les secrets
-client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
 
-if prompt:= st.text_input("Ton message :"):
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    # Display user message in chat message container
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-# Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        stream = client.chat.completions.create(
-            model=st.session_state["openai_model"],
-            messages=[
-                {"role": m["role"], "content": m["content"]}
-                for m in st.session_state.messages
-            ],
-            stream=True,
-        )
-        response = st.write_stream(stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
 
